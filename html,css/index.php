6691ead419bf5cbddf80e90ef0c46d2d2632d0a2
valencia <valencia.hartono101@gmail.com>
2024-12-05 13:22:41 +0700

    Add files via upload

---
+++ html,css/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        
+        <h1>HI</h1>
+        
+        <?php
+        echo 7*4
+
+        ?>
+    </body>
+</html>
